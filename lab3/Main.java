da31c568073cff2d5f57b140d323088cf8c572a9
BudaElisei <eliseidaniel16@gmail.com>
2023-11-12 00:21:22 +0200

    laborator 3

--- lab3/Main.java
+++ lab3/Main.java
@@ -1,16 +1,20 @@
 package lab3;
 
+import java.io.StringBufferInputStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         elementeComune();
         numarPrim();
+        fibonnaci();
+        cuvPalindrom();
 
     }
 
     public static void elementeComune(){
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Exercitul 1:");
         int d=0;
         String[] a = new String[3];
         for (int i = 0 ; i < a.length; i++){
@@ -42,7 +46,7 @@ public class Main {
 
     public static void numarPrim(){
         Scanner aurica = new Scanner(System.in);
-
+        System.out.println("Exercitul 2:");
         int cop;
 
          int[] arr = new int[10];
@@ -66,5 +70,45 @@ public class Main {
          }
 
     }
+    public static void fibonnaci(){
 
+        int suma;
+        int var1 = 0;
+        int var2 = 1;
+        Scanner citeste = new Scanner(System.in);
+        System.out.println("Exercitul 3:");
+        System.out.println("Introduceti un numar :");
+        Integer n = citeste.nextInt();
+        System.out.print(var1+" "+var2+" ");
+        do{
+            suma = var1 + var2;
+            var1 = var2;
+            var2 = suma;
+            System.out.print(suma+" ");
+
+
+        }while (suma < n);
+
+
+
+    }
+
+    public static void cuvPalindrom(){
+        Scanner input = new Scanner(System.in);
+        String cuv1 = "";
+        String cuv2 = "";
+        System.out.println("Exercitiul 4:");
+        System.out.println("Introduceti cuvantul:");
+        cuv1 =input.nextLine();
+
+        for(int i = cuv1.length() -1; i >= 0; i--){
+            cuv2 = cuv2 + cuv1.charAt(i);
+        }
+        if(cuv1.equals(cuv2)) {
+            System.out.println("Cuvantul " + (cuv1) + " este palindrom!");
+        }else{
+            System.out.println("Cuvantul "+(cuv1)+" nu este palindrom!");
+
+        }
+    }
 }
